61bc74c33776e34056f8c73648fb363147d7d508
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2023-11-29 01:59:35 -0300

    made header and his functions

--- src/scripts/system.js
+++ src/scripts/system.js
@@ -1,3 +1,20 @@
 // Write username in header
-const userObject = JSON.parse(localStorage.getItem("loggedInUser"))
-document.write(`Olá, ${userObject.username}!`)
+const userObject = JSON.parse(localStorage.getItem("loggedInUser"));
+
+const headerSelect = document.querySelector("header");
+const usernameDiv = document.createElement("div");
+usernameDiv.setAttribute('class','username-div text-white ps-3 ps-md-5');
+
+usernameDiv.innerText = `Olá, ${userObject.username}!`;
+headerSelect.appendChild(usernameDiv);
+
+    /* logout button*/
+const logoutDiv = document.createElement("div");
+
+const logoutLink = document.createElement("a");
+logoutLink.setAttribute('href', '#');
+logoutLink.textContent = "Sair";
+logoutLink.setAttribute('class','logout-div text-white pe-3 pe-md-5');
+
+logoutDiv.appendChild(logoutLink);
+headerSelect.appendChild(logoutDiv);
